f7cb56561f3af227bf4f6f5ae51277688e5ca3a2
Charles Melo <melocharles220@gmail.com>
2025-02-15 15:59:45 -0300

    feat: ad header componet

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
-
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
 const Header = () => {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between px-5 pt-6">
       <Image src="/logo.png" alt="fws foods" height={30} width={100} />
+      <Button
+        size="icon"
+        variant="outline"
+        className="border-none bg-transparent"
+      >
+        <MenuIcon />
+      </Button>
     </div>
   );
 };
